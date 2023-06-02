38df13478ef47768f318b461b89e2fbe65aac17b
Ashley <AshWrightA@Outlook.com>
2023-06-01 19:29:30 -0600

    Finished exercises 1-7

--- src/main/java/org/comit/comit_practice/App.java
+++ src/main/java/org/comit/comit_practice/App.java
@@ -4,10 +4,42 @@ package org.comit.comit_practice;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		exerciseOne();
+		exerciseTwo();
+		exerciseThree();
+		exerciseFour();
+		exerciseFive();
+		exerciseSix();
+		exerciseSeven();
+	}
+
+	public static void exerciseOne() {
+		System.out.println("Hi, I already print sentances");
+	}
+
+	public static void exerciseTwo() {
+		System.out.println("273");
+	}
+
+	public static void exerciseThree() {
+		System.out.println(5.3f);
+	}
+
+	public static void exerciseFour() {
+		System.out.println(1234 + 532);
+	}
+
+	public static void exerciseFive() {
+		System.out.println(1234 - 532);
+	}
+
+	public static void exerciseSix() {
+		System.out.println(1234 * 532);
+	}
+
+	public static void exerciseSeven() {
+		System.out.println((float) 1234 / 532);
+	}
 }
